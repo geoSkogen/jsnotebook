7dfdf0b7f4f1e7f365bf06b71581eaefe5e5ae17
geoSkogen <borninthecaul@gmail.com>
2018-10-20 08:15:33 -0700

    fixes button_links.js for index page, adds titles

--- scripts/button_links.js
+++ scripts/button_links.js
@@ -3,12 +3,17 @@ window.addEventListener('load', initClick);
 function initClick() {
 
   var href;
+  var this_a;
+  var but_index = 0
   var buts = document.getElementsByClassName("button");
   var as = document.getElementsByTagName("a");
 
   for (var i = 0; i < as.length; i++) {
-    href = as[i].getAttribute("href");
-    clickLink(buts[i], href);
+    if (as[i].className != "titleLink") {
+      href = as[i].getAttribute("href");
+      clickLink(buts[but_index], href);
+      but_index++;
+    }
   }
 
   function clickLink(elm, url) {
